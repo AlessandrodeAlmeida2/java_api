7ec685ea2b65ccb31c1dc4e50e6e9f4af46312ad
AlessandrodeAlmeida2 <agostini55@gmail.com>
2024-02-02 13:45:41 -0300

    Class user

---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,20 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+
+    @Column(unique =true)
+    private String email;
+    private String password;
+}
