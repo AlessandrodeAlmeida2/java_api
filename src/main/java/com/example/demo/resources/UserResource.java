ea138ffa8856665b89961d1496b57721140043ee
AlessandrodeAlmeida2 <agostini55@gmail.com>
2024-02-03 10:09:48 -0300

    class UserResource

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,6 +1,8 @@
-package resources;
+package com.example.demo.resources;
 
 import com.example.demo.domain.AppUser;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
+    @Autowired
+    private UserService service;
     @GetMapping(value = "/{id}")
     public ResponseEntity<AppUser> findById(@PathVariable Integer id){
 
-        return ResponseEntity.ok().body(new AppUser(1, "Valter", "almeida@email.com", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
